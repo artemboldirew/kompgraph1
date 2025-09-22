3dc1b9d9f18f35d523c34fa2038244135836e06c
neeby <artemboldirew2@gmail.com>
2025-09-22 20:48:09 +0300

    branch test

--- src/main/java/boldyrev/model/shape/RoadSign.java
+++ src/main/java/boldyrev/model/shape/RoadSign.java
@@ -11,15 +11,16 @@ public class RoadSign extends Shape {
 
     @Override
     public void draw(Graphics2D g) {
-        Color signColor = new Color(139, 127, 79);
-        g.setColor(signColor);
-        g.fillRect(x, y, width, getVal(0.66));
-        g.fillRect(x + (width - getVal(4.0/50))/2, y + getVal(0.66), getVal(4.0/50), getVal(15.0/50));
-        String text1 = "С новым";
-        String text2 = "годом!";
-        g.setFont(new Font("Arial", Font.BOLD, 8));
-        g.setColor(Color.BLACK); // Цвет текста
-        g.drawString(text1, x + getVal(0.18), y + getVal(0.28));
-        g.drawString(text2, x + getVal(0.27) , y + getVal(0.48));
+//        Color signColor = new Color(139, 127, 79);
+//        g.setColor(signColor);
+//        g.fillRect(x, y, width, getVal(0.66));
+//        g.fillRect(x + (width - getVal(4.0/50))/2, y + getVal(0.66), getVal(4.0/50), getVal(15.0/50));
+//        String text1 = "С новым";
+//        String text2 = "годом!";
+//        g.setFont(new Font("Arial", Font.BOLD, 8));
+//        g.setColor(Color.BLACK); // Цвет текста
+//        g.drawString(text1, x + getVal(0.18), y + getVal(0.28));
+//        g.drawString(text2, x + getVal(0.27) , y + getVal(0.48));
+        //blablablalbla
     }
 }
